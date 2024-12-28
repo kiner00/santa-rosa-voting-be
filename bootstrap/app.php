c3469ef691307e59142fc0c40e19f9b53415a5c3
kiner00 <kinermercurio@gmail.com>
2024-12-28 13:33:55 +0800

    remove global middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(IsAdminMiddleware::class);
+        // $middleware->append(IsAdminMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
